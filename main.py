3c357f0fa1a3aa6351fcae051f3ea1c6f31a0b1d
Nikolay Shirokov <nshirokov@internet.ru>
2023-11-22 21:03:54 +0700

    add ephem

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import settings
+import ephem
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
 PROXY = {'proxy_url': settings.PROXY_URL}
 
+today = '2023/11/21'
+planet_dict = {'Mars': ephem.Mars(today), 'Venus': ephem.Venus(today), 'Saturn': ephem.Saturn(today), 'Jupiter': ephem.Jupiter(today),
+               'Neptune': ephem.Neptune(today), 'Uranus': ephem.Uranus(today), 'Mercury': ephem.Mercury(today)}
+
 def greet_user(update, context):
     print('Вызван /start')
     update.message.reply_text('Здравствуй, пользователь!')
+
+def planet_user(update, context):
+    print('Вызван /planet')
+    planet_name = update.message.text.split()[1]
+    print(f'Введена планета {planet_name}')
+    planet = planet_dict.get(planet_name)
+    if planet != None:
+        constellation = ephem.constellation(planet_dict[planet_name])
+        update.message.reply_text(constellation[1])
+    else:
+        update.message.reply_text('Планета не найдена!')
 
 def talk_to_me(update, context):
     text = update.message.text
@@ -20,11 +36,13 @@ def main():
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('planet', planet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
 
     logging.info('Бот стартовал')
     mybot.start_polling()
     mybot.idle()
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     main()
