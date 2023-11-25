39a2fc4b082dd6fea9a8fe7e1df8b551005fe277
Nikolay Shirokov <nshirokov@internet.ru>
2023-11-25 13:20:44 +0700

    fixed work

--- main.py
+++ main.py
@@ -5,20 +5,18 @@ import ephem
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
-PROXY = {'proxy_url': settings.PROXY_URL}
-
 today = '2023/11/21'
 planet_dict = {'Mars': ephem.Mars(today), 'Venus': ephem.Venus(today), 'Saturn': ephem.Saturn(today), 'Jupiter': ephem.Jupiter(today),
                'Neptune': ephem.Neptune(today), 'Uranus': ephem.Uranus(today), 'Mercury': ephem.Mercury(today)}
 
 def greet_user(update, context):
-    print('Вызван /start')
+    logging.info('Вызван /start')
     update.message.reply_text('Здравствуй, пользователь!')
 
 def planet_user(update, context):
-    print('Вызван /planet')
+    logging.info('Вызван /planet')
     planet_name = update.message.text.split()[1]
-    print(f'Введена планета {planet_name}')
+    logging.info(f'Введена планета {planet_name}')
     planet = planet_dict.get(planet_name)
     if planet != None:
         constellation = ephem.constellation(planet_dict[planet_name])
