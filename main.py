2184c222a91def6800728eec8607932a66520209
Nikolay Shirokov <nshirokov@internet.ru>
2023-12-06 21:41:21 +0700

    refactoring and add keyboard

--- main.py
+++ main.py
@@ -1,75 +1,10 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import settings
-import ephem
-from datetime import datetime
-from random import randint, choice
-from glob import glob
-from emoji import emojize
+from handlers import greet_user,planet_user,guess_number,send_picture,user_coordinates,talk_to_me
+
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
-
-
-today = datetime.now()
-planet_dict = {'Mars': ephem.Mars(today), 'Venus': ephem.Venus(today), 'Saturn': ephem.Saturn(today), 'Jupiter': ephem.Jupiter(today),
-               'Neptune': ephem.Neptune(today), 'Uranus': ephem.Uranus(today), 'Mercury': ephem.Mercury(today)}
-
-def greet_user(update, context):
-    logging.info('Вызван /start')
-    context.user_data['emoji'] = get_smile(context.user_data)
-    update.message.reply_text(f"Здравствуй, пользователь {context.user_data['emoji']}!")
-
-def planet_user(update, context):
-    logging.info('Вызван /planet')
-    planet_name = update.message.text.split()[1]
-    logging.info(f'Введена планета {planet_name}')
-    planet = planet_dict.get(planet_name)
-    if planet != None:
-        constellation = ephem.constellation(planet_dict[planet_name])
-        update.message.reply_text(constellation[1])
-    else:
-        update.message.reply_text('Планета не найдена!')
-
-def guess_number(update, context):
-    logging.info('Вызван /guess')
-    if context.args:
-        try:
-            user_number = int(context.args[0])
-            message = play_random_numbers(user_number)
-        except (TypeError, ValueError):
-            message = "Введите целое число"
-    else:
-        message = "Введите целое число"
-    update.message.reply_text(message)
-
-def play_random_numbers(user_number):
-    bot_number = randint(user_number-10, user_number+10)
-    if user_number > bot_number:
-        message = f"Ты загадал {user_number}, я загадал {bot_number}, ты выиграл!"
-    elif user_number == bot_number:
-        message = f"Ты загадал {user_number}, я загадал {bot_number}, ничья!"
-    else:
-        message = f"Ты загадал {user_number}, я загадал {bot_number}, я выиграл!"
-    return message
-
-def send_picture(update, context):
-    logging.info('Вызван /pictures')
-    photos_list = glob('images/*.jp*g')
-    pic_filename = choice(photos_list)
-    chat_id = update.effective_chat.id
-    context.bot.send_photo(chat_id=chat_id, photo=open(pic_filename, 'rb'))
-
-def talk_to_me(update, context):
-    context.user_data['emoji'] = get_smile(context.user_data)
-    text = update.message.text
-    logging.info(f'Введен текст: {text}')
-    update.message.reply_text(f"{text} {context.user_data['emoji']}")
-
-def get_smile(user_data):
-    if 'emoji' not in user_data:
-        smile = choice(settings.USER_EMOJI)
-        return emojize(smile, language='alias')
-    return user_data['emoji']
 
 def main():
     mybot = Updater(settings.API_KEY, use_context = True)
@@ -79,6 +14,8 @@ def main():
     dp.add_handler(CommandHandler('planet', planet_user))
     dp.add_handler(CommandHandler("guess", guess_number))
     dp.add_handler(CommandHandler("pictures", send_picture))
+    dp.add_handler(MessageHandler(Filters.regex('^(Прислать картинку)$'), send_picture))
+    dp.add_handler(MessageHandler(Filters.location, user_coordinates))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
 
